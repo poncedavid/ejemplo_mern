244409c219a44d9ab5d981184f86502613863c67
“poncedavid” <Da.ponce@duocuc.cl>
2024-03-21 22:56:49 -0300

    Actualiza el puerto de escucha del servidor a 8080

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,12 @@ const app = express();
 app.use(cors()); // Habilitamos el acceso a la API desde cualquier origen
 app.use(express.json()); // Habilitamos el uso de JSON en la API
 app.use(petRoutes.petRouter); // Habilitamos las rutas de la API
-app.listen(6000); // Levantamos el servidor en el puerto 8080
+app.listen(8080); // Levantamos el servidor en el puerto 8080
 
 
 //Conexión a la base de datos
 mongoose.connect("mongodb://127.0.0.1:27017/mascotita")
-.then(()=>console.log("Conexion Correcta:[ puerto 6000 ]"))
+.then(()=>console.log("Conexion Correcta:[ puerto 8080 ]"))
 .catch((e)=>console.log("Error" + e));
+
 
